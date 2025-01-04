4a6a97ca0cbd97589086c5d2196bc7641508539d
KsRawat <rawatks192@gmail.com>
2025-01-04 21:19:50 +0530

    linked-list implementation

--- Data Structure/linkedList/singleLL/linkedList.cpp
+++ Data Structure/linkedList/singleLL/linkedList.cpp
@@ -10,12 +10,15 @@ struct Node{
 
 class LinkedList{
     Node* head;
+    int length=0;
+
     public:
 
     LinkedList() : head(nullptr) {}
     // insertion
     void insertAtBeg(int val){
         Node* newNode = new Node(val);
+        length++;
         if(head==nullptr){
             head = newNode;
             return;
@@ -23,9 +26,9 @@ class LinkedList{
         newNode->next = head;
         head = newNode;
     }
-
     void insertAtEnd(int val){
         Node* newNode = new Node(val);
+        length++;
         if(head==nullptr){
             head = newNode;
             return;
@@ -36,11 +39,34 @@ class LinkedList{
         }
         curr->next = newNode;
     }
+    void insertAtPosition(int val, int pos){
+        if(pos<1 || pos>length + 1){
+            cout << "Invalid position!" << endl; 
+            return; 
+        }
+        if(pos==1){
+            insertAtBeg(val);
+            return;
+        }
+        if(pos==length+1){
+            insertAtEnd(val);
+            return;
+        }
+        Node* newNode = new Node(val);
+        length++;   
+        Node* curr = head;
+        for(int i=2; i<pos; i++){
+            curr = curr->next;
+        }
+        newNode->next = curr->next;
+        curr->next = newNode;
+    }
 
     int deleteAtBeg(){
         if(head == nullptr){
             return -1;
         }
+        length--;
         Node* temp = head;
         head = head->next;
         int deleting = temp->data;
@@ -51,6 +77,7 @@ class LinkedList{
         if(head == nullptr){
             return -1;
         }
+        length--;
         Node* curr = head;
         while(curr->next->next!=nullptr){
             curr = curr->next;
@@ -61,8 +88,40 @@ class LinkedList{
         delete temp;
         return deleting;
     }
+    int deleteAtPosition(int pos){
+        if(pos<1 || pos>length){
+            cout << "Invalid position!" << endl;
+            return -1; 
+        }
+        if(pos==1){
+            return deleteAtBeg();
+        }
+        if (pos==length+1){
+            return deleteAtEnd();
+        }
+        Node* curr = head;
+        Node* temp;
+        for(int i=2; i<pos; i++){
+            curr = curr->next;
+        }
+        temp = curr->next;
+        curr->next = curr->next->next;
+        int deleting = temp->data;
+        temp->next = nullptr;
+        delete temp;
+        length--;
+        return deleting;
+    }
 
-    void print(){
+    int getLength(){
+        return length;
+    }
+
+    bool isEmpty(){
+        return length==0;
+    }
+
+    void printList(){
         if(head==nullptr){
             return;
         }
@@ -74,23 +133,40 @@ class LinkedList{
         cout<<endl;
     }
 };
-
 int main(){
     LinkedList ll;
 
-    ll.insertAtBeg(10);
-    ll.insertAtBeg(20);
-    ll.insertAtBeg(30);
-
-    ll.insertAtEnd(-20);
-    ll.insertAtEnd(-30);
-
-    ll.print();
-
-    ll.deleteAtBeg();
-    ll.deleteAtEnd();
-
-    ll.print();
+    cout << "Inserting elements at the beginning:" << endl; 
+    ll.insertAtBeg(10); 
+    ll.insertAtBeg(20); 
+    ll.printList(); 
+    // Output: 20 10 
+    cout<<"Inserting elements at the end:"<<endl;
+    ll.insertAtEnd(30); 
+    ll.insertAtEnd(40); 
+    ll.printList(); 
+    // Output: 20 10 30 40 
+    cout<<"Inserting element at position 3:"<<endl; 
+    ll.insertAtPosition(25, 3); 
+    ll.printList(); 
+    // Output: 20 10 25 30 40 
+    cout<<"Deleting the first element:"<<endl; 
+    ll.deleteAtBeg(); 
+    ll.printList(); 
+    // Output: 10 25 30 40 
+    cout<<"Deleting the last element:"<<endl; 
+    ll.deleteAtEnd(); 
+    ll.printList(); 
+    // Output: 10 25 30 
+    cout<<"Deleting element at position 2:"<<endl; 
+    ll.deleteAtPosition(2); 
+    ll.printList(); 
+    // Output: 10 30 
+    cout<<"Length of the list: " << 
+    ll.getLength()<<endl; 
+    // Output: 2 
+    cout<<"Is the list empty?: " << (ll.isEmpty() ? "Yes" : "No")<<endl; 
+    // Output: No 
 
     return 0;
 }
