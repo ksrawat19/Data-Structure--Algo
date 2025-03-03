7214294089af3621c124eee34138044c27dabb1e
KsRawat <rawatks192@gmail.com>
2025-03-03 05:57:15 +0000

    house robber with more methods

--- Solving Lab/dp/houseRobber.cpp
+++ Solving Lab/dp/houseRobber.cpp
@@ -23,7 +23,40 @@ public:
         return pickOrNot(nums, 0, dp);
     }
 };
+// Using tabulation
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size()-1;
+        vector<int> v(n+2, 0);
+        v[n] = nums[n];
 
+        for(int i=n-1; i>=0; i--){
+            int include = nums[i] + v[i+2];
+            int exclude = v[i+1];
+            v[i] = max(include, exclude);
+        }
+        return v[0];
+    }
+};
+// space optimization
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size()-1;    
+        int last = nums[n];
+        int lastNext = 0;
+
+        for(int i=n-1; i>=0; i--){
+            int include = nums[i] + lastNext;
+            int exclude = last;
+            nums[i] = max(include, exclude);
+            lastNext = last;
+            last = nums[i];
+        }
+        return nums[0];
+    }
+};
 int main() {
     Solution solution;
     vector<int> nums = {2, 7, 9, 3, 1};
