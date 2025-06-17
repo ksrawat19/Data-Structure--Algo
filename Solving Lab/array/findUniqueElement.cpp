a898ac89ea9175b705e8e3c42e65a137f8c7384c
KsRawat <rawatks192@gmail.com>
2025-06-17 22:33:43 +0530

    find unique method added

--- Solving Lab/array/findUniqueElement.cpp
+++ Solving Lab/array/findUniqueElement.cpp
@@ -4,9 +4,10 @@
 #include <vector>
 using namespace std;
 
+// NOTE: only work if one unique element is present in the array, if more than one unique the map will useful!!
 int uniqueElement(int *arr, int _size){
 
-    // Method 1
+    // // Method 1
     // for(int i=0; i<_size-1; i++){
     //     bool isUnique = true;
     //     for (int j = 0; j < _size; j++) {
@@ -22,23 +23,30 @@ int uniqueElement(int *arr, int _size){
     // return -1;
     
 
-    // Method 2
-    sort(arr, arr+_size);
+    // // Method 2
     // 2,2,10,10,11,12,12,16,16
-    vector<int> ans(_size, 0);
+    vector<int> v(arr, arr + _size);
+    sort(v.begin(), v.end());
 
-    for(int i=0; i<_size; i++){
-        cout<<ans[i]<<" ";
+    for (int i = 0; i < _size; i++) {
+        if ((i == 0 || v[i] != v[i - 1]) &&
+            (i == _size - 1 || v[i] != v[i + 1])) {
+            return v[i];
+        }
     }
-    cout<<endl;
-
-
-
-    // Method 3
+    return -1;
+    
+    
+    // // Method 3 (using xor)
+    // int unique = 0;
+    // for(int i=0; i<_size; i++){
+    //     unique ^= arr[i];
+    // }
+    // return (unique)? unique: -1;
 }
 
 int main(){
-    int arr[] = {2,10,11,13,10,2,16,13,16};
+    int arr[] = {2,13,10,2,16,13,16};
     int size = sizeof(arr)/sizeof(arr[0]);
 
     for(auto i: arr){
